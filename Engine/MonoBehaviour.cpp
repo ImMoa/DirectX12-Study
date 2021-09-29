eabe6ea0cc09310d3f16ed14da68c4051cfe7b48
ImMoa <83941587+ImMoa@users.noreply.github.com>
2021-09-29 15:58:22 +0900

    초기 커밋.

---
+++ Engine/MonoBehaviour.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "MonoBehaviour.h"
+#include "Component.h"
+
+MonoBehaviour::MonoBehaviour() : Component(COMPONENT_TYPE::MONO_BEHAVIOUR)
+{
+
+}
+
+MonoBehaviour::~MonoBehaviour()
+{
+
+}
